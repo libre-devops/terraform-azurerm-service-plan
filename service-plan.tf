103f3429b32a836f1e67673cdd29b719d8ab5a5a
Craig Thacker <craigthackerx@gmail.com>
2022-04-30 18:34:28 +0100

    Update Module

--- service-plan.tf
+++ service-plan.tf
@@ -5,9 +5,9 @@ resource "azurerm_service_plan" "plan" {
   resource_group_name          = var.rg_name
   app_service_environment_id   = var.add_to_app_service_environment ? var.app_service_environment_id : false
   maximum_elastic_worker_count = var.maximum_elastic_worker_count
-  per_site_scaling             = var.per_site_scaling
   zone_balancing_enabled       = var.zone_balancing_enabled
 
+  os_type  = var.os_type
   sku_name = var.sku_name
 
   tags = var.tags
